82878ff1a73b57f6d478977f636ef2e7dfa52859
Michiel <michielp1807@gmail.com>
2025-07-16 11:58:07 +0200

    Use proper dependency imports

--- default.nix
+++ default.nix
@@ -1,25 +1,30 @@
-{ pkgs ? import <nixpkgs> {} }:
+{
+  lib,
+  fetchFromGitHub,
+  python3,
+  rustPlatform,
+}:
 
-pkgs.python3Packages.buildPythonPackage rec {
+python3.pkgs.buildPythonPackage rec {
   pname = "ziplinter";
   version = "0.1.0";
   pyproject = true;
 
-  src = pkgs.fetchFromGitHub {
+  src = fetchFromGitHub {
     owner = "trifectatechfoundation";
     repo = "ziplinter";
     rev = "698922aff67194f511da0586433504cdf43fe965";
     hash = "sha256-YL41HUoQfc9StAAHBR0Gt7r5NFQsh6LjfdFfiYRNB4s=";
   };
 
-  cargoDeps = pkgs.rust.packages.stable.rustPlatform.fetchCargoVendor {
+  cargoDeps = rustPlatform.fetchCargoVendor {
     inherit pname version src;
     hash = "sha256-RjMp+9VfIalGcDGLdncYg/6KjIodR/9IMGQZw9/g2EM=";
   };
 
   buildAndTestSubdir = "ziplinter-python";
 
-  nativeBuildInputs = with pkgs.rust.packages.stable.rustPlatform; [
+  nativeBuildInputs = with rustPlatform; [
     cargoSetupHook
     maturinBuildHook
   ];
@@ -27,15 +32,15 @@ pkgs.python3Packages.buildPythonPackage rec {
   meta = {
     description = "A zip file analyzer Python module";
     homepage = "https://github.com/trifectatechfoundation/ziplinter";
-    license = with pkgs.lib.licenses; [
+    license = with lib.licenses; [
       asl20
       mit
     ];
-    maintainers = with pkgs.lib.maintainers; [
+    maintainers = with lib.maintainers; [
       folkertdev
       michielp1807
       armijnhemel
     ];
-    platforms = pkgs.lib.platforms.all;
+    platforms = lib.platforms.all;
   };
 }
